56189df8a184744b07a2f543fe60933cd8a6b7f3
LoverofDarkness <loverofdarkness@mail2indian.com>
2026-09-10 22:59:33 +0530

    Add guarded recovery executor package

---
+++ devil/recovery/__init__.py
@@ -0,0 +1,5 @@
+"""Guarded recovery execution primitives."""
+
+from devil.recovery.executor import RecoveryError, RecoveryExecutor, RepairReport
+
+__all__ = ["RecoveryError", "RecoveryExecutor", "RepairReport"]
